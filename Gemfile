8248f626535899f67674cece65b375582428b9e2
Frizlab <fload@me.com>
2022-03-29 14:53:16 +0300

    Drop cocoapods

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem "slather", "2.6.1"
-gem "cocoapods"
-gem "jazzy"
--- Package.swift
+++ Package.swift
@@ -1,35 +1,22 @@
 // swift-tools-version:5.1
-
 import PackageDescription
 
+
 let package = Package(
-    name: "ChatLayout",
-    platforms: [
-        .iOS(.v12)
-    ],
-    products: [
-        .library(
-            name: "ChatLayout",
-            targets: ["ChatLayout"]
-        ),
-        .library(name: "ChatLayoutStatic",
-                 type: .static,
-                 targets: ["ChatLayout"]),
-        .library(name: "ChatLayoutDynamic",
-                 type: .dynamic,
-                 targets: ["ChatLayout"])
-    ],
-    targets: [
-        .target(
-            name: "ChatLayout",
-            dependencies: [],
-            path: "ChatLayout/Classes"
-        ),
-        .testTarget(
-            name: "ChatLayoutTests",
-            dependencies: ["ChatLayout"],
-            path: "Example/Tests"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+	name: "ChatLayout",
+	platforms: [
+		.iOS(.v12)
+	],
+	products: [
+		.library(name: "ChatLayout",                        targets: ["ChatLayout"]),
+		.library(name: "ChatLayoutStatic",  type: .static,  targets: ["ChatLayout"]),
+		.library(name: "ChatLayoutDynamic", type: .dynamic, targets: ["ChatLayout"])
+	],
+	targets: [
+		.target(name: "ChatLayout"),
+		.testTarget(
+			name: "ChatLayoutTests",
+			dependencies: ["ChatLayout"]
+		)
+	]
 )
